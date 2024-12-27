04d31e61b0132175866a04b76f194a2fa332707e
Pineapple-Soup <91655764+Pineapple-Soup@users.noreply.github.com>
2024-12-26 17:52:17 -0800

    build: integrate projects section

---
+++ src/components/Projects.tsx
@@ -0,0 +1,7 @@
+export default function Projects() {
+    return (
+        <section id="projects" className="w-full h-screen flex flex-col justify-center">
+            <h2 className="mx-auto"> Projects </h2>
+        </section>
+    );
+}
