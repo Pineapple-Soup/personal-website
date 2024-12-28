6c2d091f3dee840fa748d00d426c877e18d354ee
Pineapple-Soup <91655764+Pineapple-Soup@users.noreply.github.com>
2024-12-28 01:40:30 -0800

    build: change vertical spacing

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -47,8 +47,8 @@ export default function Projects() {
     ];
 
     return (
-        <section id="projects" className="w-full h-screen flex flex-col justify-center items-center">
-            <h2 className="my-10"> PROJECTS </h2>
+        <section id="projects" className="w-full h-screen flex flex-col items-center">
+            <h2 className="my-12"> PROJECTS </h2>
             <sub className="mb-20"> Click the cards to explore my work </sub>
             <Carousel cards={cards} />
         </section>
