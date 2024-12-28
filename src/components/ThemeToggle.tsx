c2cd001d9c5e1be0dabdb7c3777cdf81aa5a010d
Pineapple-Soup <91655764+Pineapple-Soup@users.noreply.github.com>
2024-12-28 00:14:00 -0800

    fix: use SSR when component is not mounted

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -10,11 +10,7 @@ export default function ThemeToggle() {
 
     useEffect(() => { setMounted(true); }, []);
 
-    if (!mounted) return window.matchMedia('(prefers-color-scheme: dark)').matches ? (
-        <BsSunFill className="cursor-pointer my-auto size-10 icon-transition" onClick={() => setTheme("light")} />
-        ) : (
-        <BsFillMoonStarsFill className="cursor-pointer my-auto size-10 icon-transition" onClick={() => setTheme("dark")} />
-        );
+    if (!mounted) return (<BsSunFill className="cursor-pointer my-auto size-10 icon-transition"/>);
 
     return (
         (theme === "dark" || (theme === "system" && window.matchMedia('(prefers-color-scheme: dark)').matches)) ? (
