2e98e9a2a92ffc55ba9d4951bd5fd974f5e1f97c
Pineapple-Soup <91655764+Pineapple-Soup@users.noreply.github.com>
2024-12-28 16:58:28 -0800

    refactor: extract contact form to separate component

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import ContactForm from "@/components/ContactForm";
 import { MdCellTower, MdLocationPin, MdMail } from "react-icons/md";
 import { GiConversation } from "react-icons/gi";
 
@@ -7,16 +7,7 @@ export default function Contact() {
         <section id="contact" className="w-full h-screen grid grid-rows-6 grid-cols-12 gap-4">
             <div className="bg-primary flex flex-col justify-evenly rounded-3xl row-start-2 row-end-6 col-start-1 col-end-7">
                 <h2 className="mx-auto"> Get In Touch! </h2>
-                <form className="flex flex-col p-4">
-                    <div className="flex space-x-4">
-                        <input type="text" placeholder="First Name" className="bg-secondary focus:outline-none focus:ring-accent focus:ring-4 placeholder-slate-600 rounded-lg p-2 my-2 w-1/2"/>
-                        <input type="text" placeholder="Last Name" className="bg-secondary focus:outline-none focus:ring-accent focus:ring-4 placeholder-slate-600 rounded-lg p-2 my-2 w-1/2"/>
-                    </div>
-                    <input type="email" placeholder="Email" className="bg-secondary focus:outline-none focus:ring-accent focus:ring-4 placeholder-slate-600 rounded-lg p-2 my-2"/>
-                    <input type="subject" placeholder="Subject" className="bg-secondary focus:outline-none focus:ring-accent focus:ring-4 placeholder-slate-600 rounded-lg p-2 my-2"/>
-                    <textarea placeholder="Message" className="bg-secondary focus:outline-none focus:ring-accent focus:ring-4 placeholder-slate-600 resize-none rounded-lg p-2 my-2" rows={5} cols={40}/>
-                    <button type="submit" className="bg-secondary hover:bg-accent rounded-lg p-2 my-2">Submit</button>
-                </form>
+                <ContactForm/>
             </div>
             <div className="flex flex-col justify-evenly bg-primary rounded-3xl row-start-4 row-end-6 col-start-7 col-span-4">
                 <h3 className="mx-auto font-bold"> Contact Information </h3>
---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,14 @@
+export default function ContactForm() {
+    return (
+        <form className="flex flex-col p-4">
+            <div className="flex space-x-4">
+                <input type="text" placeholder="First Name" className="bg-secondary focus:outline-none focus:ring-accent focus:ring-4 placeholder-slate-600 rounded-lg p-2 my-2 w-1/2"/>
+                <input type="text" placeholder="Last Name" className="bg-secondary focus:outline-none focus:ring-accent focus:ring-4 placeholder-slate-600 rounded-lg p-2 my-2 w-1/2"/>
+            </div>
+            <input type="email" placeholder="Email" className="bg-secondary focus:outline-none focus:ring-accent focus:ring-4 placeholder-slate-600 rounded-lg p-2 my-2"/>
+            <input type="subject" placeholder="Subject" className="bg-secondary focus:outline-none focus:ring-accent focus:ring-4 placeholder-slate-600 rounded-lg p-2 my-2"/>
+            <textarea placeholder="Message" className="bg-secondary focus:outline-none focus:ring-accent focus:ring-4 placeholder-slate-600 resize-none rounded-lg p-2 my-2" rows={5} cols={40}/>
+            <button type="submit" className="bg-secondary hover:bg-accent rounded-lg p-2 my-2">Submit</button>
+        </form>
+    );
+}
