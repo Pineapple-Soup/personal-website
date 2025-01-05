946fcf82053ad4b9e564b334d844e55a913fdfd4
Pineapple-Soup <91655764+Pineapple-Soup@users.noreply.github.com>
2025-01-05 03:26:11 -0800

    feat: add drop shadow to button

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,7 +7,7 @@ export default function Projects() {
             <h2 className="mt-8"> PROJECTS </h2>
             <sub className=""> Click the cards to explore my work </sub>
             <Carousel />
-            <button className="bg-secondary text-lg hover:bg-accent hover:scale-110 rounded-lg p-2 my-2">
+            <button className="bg-secondary text-lg drop-shadow-xl hover:bg-accent hover:scale-110 rounded-lg p-2 my-2">
                 <Link href="/projects"> View All Projects </Link>
             </button>
         </section>
