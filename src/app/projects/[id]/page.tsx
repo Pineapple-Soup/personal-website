a4c93cd3cfc06829dddc4a7feeb1a82db8787191
Pineapple-Soup <91655764+Pineapple-Soup@users.noreply.github.com>
2025-01-05 03:16:40 -0800

    feat: dynamically display project title on subpages

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,11 +1,12 @@
 import Link from 'next/link';
 import ThemeToggle from '@/components/ThemeToggle';
+import project_data from '@/data/projects.json';
 
 export default async function ProjectPage({ params }: { params: Promise<{ id: number }> }) {
 
     const id = (await params).id;
-    const name = "Project " + id;
-    
+    const title = project_data[id].title;
+
     return (
         <main>
             <header className="navbar">
@@ -15,8 +16,9 @@ export default async function ProjectPage({ params }: { params: Promise<{ id: nu
                 </button>
             </header>
             <section className="w-full h-screen flex flex-col justify-center items-center">
-                <h2 className="text-4xl font-bold"> {name} </h2>
-                <p className="my-12"> Work in progress... </p>
+                <h2 className="text-4xl font-bold"> {title} </h2>
+                <p className="my-12"> This page is a work in progress... </p>
+                <p> (but you can check out the project my github!) </p>
             </section>
         </main>
     );
