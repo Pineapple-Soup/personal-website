457ce60ece60fdb6c23b338256c230f4a0242e00
Pineapple-Soup <91655764+Pineapple-Soup@users.noreply.github.com>
2025-01-05 02:44:51 -0800

    feat: add button to view all projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,11 +1,15 @@
 import Carousel from "@/components/Carousel";
+import Link from "next/link";
 
 export default function Projects() {
     return (
-        <section id="projects" className="w-full h-screen flex flex-col items-center">
-            <h2 className="my-12"> PROJECTS </h2>
-            <sub className="mb-20"> Click the cards to explore my work </sub>
+        <section id="projects" className="w-full h-screen flex flex-col justify-evenly items-center">
+            <h2 className="mt-8"> PROJECTS </h2>
+            <sub className=""> Click the cards to explore my work </sub>
             <Carousel />
+            <button className="bg-secondary text-lg hover:bg-accent hover:scale-110 rounded-lg p-2 my-2">
+                <Link href="/projects"> View All Projects </Link>
+            </button>
         </section>
     );
 }
