c5269c6c50fa79b6930dd20342e6a7b452b50da5
Pineapple-Soup <91655764+Pineapple-Soup@users.noreply.github.com>
2025-08-06 23:26:30 -0700

    build: update tailwindcss to v4.1.11

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -5,27 +5,27 @@ export default function ContactForm() {
         <input
           type='text'
           placeholder='First Name'
-          className='bg-secondary focus:outline-none focus:ring-accent focus:ring-4 placeholder-slate-600 rounded-lg p-2 my-2 w-1/2'
+          className='bg-secondary focus:outline-hidden focus:ring-accent focus:ring-4 placeholder-slate-600 rounded-lg p-2 my-2 w-1/2'
         />
         <input
           type='text'
           placeholder='Last Name'
-          className='bg-secondary focus:outline-none focus:ring-accent focus:ring-4 placeholder-slate-600 rounded-lg p-2 my-2 w-1/2'
+          className='bg-secondary focus:outline-hidden focus:ring-accent focus:ring-4 placeholder-slate-600 rounded-lg p-2 my-2 w-1/2'
         />
       </div>
       <input
         type='email'
         placeholder='Email'
-        className='bg-secondary focus:outline-none focus:ring-accent focus:ring-4 placeholder-slate-600 rounded-lg p-2 my-2'
+        className='bg-secondary focus:outline-hidden focus:ring-accent focus:ring-4 placeholder-slate-600 rounded-lg p-2 my-2'
       />
       <input
         type='subject'
         placeholder='Subject'
-        className='bg-secondary focus:outline-none focus:ring-accent focus:ring-4 placeholder-slate-600 rounded-lg p-2 my-2'
+        className='bg-secondary focus:outline-hidden focus:ring-accent focus:ring-4 placeholder-slate-600 rounded-lg p-2 my-2'
       />
       <textarea
         placeholder='Message'
-        className='bg-secondary focus:outline-none focus:ring-accent focus:ring-4 placeholder-slate-600 resize-none rounded-lg p-2 my-2'
+        className='bg-secondary focus:outline-hidden focus:ring-accent focus:ring-4 placeholder-slate-600 resize-none rounded-lg p-2 my-2'
         rows={5}
         cols={40}
       />
